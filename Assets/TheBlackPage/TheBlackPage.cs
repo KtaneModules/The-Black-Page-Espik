14d08e8c7549114e126f7e712e1a00242d9b12be
Espik_23 <sbick23@outlook.com>
2020-04-16 15:31:57 -0500

    Version v1.101

--- Assets/TheBlackPage/TheBlackPage.cs
+++ Assets/TheBlackPage/TheBlackPage.cs
@@ -122,7 +122,7 @@ public class TheBlackPage : MonoBehaviour {
 
     // Turns the sheet music off after the lights turn on
     public IEnumerator FadeSheetMusic() {
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(3.0f);
         ToggleSheetMusic(false);
         Lights[0].enabled = false;
         Lights[1].enabled = false;
